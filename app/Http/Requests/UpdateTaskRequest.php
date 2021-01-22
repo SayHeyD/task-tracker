7376c0fbbdc4ef0ab287ed5c3cd5853d345bf6a1
David <david@docampo.ch>
2021-01-23 00:49:11 +0100

    added UpdateTaskRequest

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:255'],
+            'description' => ['nullable'],
+            'complete' => ['nullable'],
+            'due_at' => ['nullable', 'date_format:Y-m-d H:i:s'],
+            'user_id' => ['required'],
+        ];
+    }
+}
