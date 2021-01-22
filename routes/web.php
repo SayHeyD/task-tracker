d88b24768273ce3cbae646a1b227275193fe68c8
David <david@docampo.ch>
2021-01-22 21:56:41 +0100

    added routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -27,4 +28,13 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
    Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
+
+   // Task routes
+   Route::prefix('/tasks')->group(function () {
+       Route::get('', [TaskController::class, 'index'])->name('tasks.index');
+       Route::put('', [TaskController::class, 'create'])->name('tasks.create');
+       Route::get('/{task}', [TaskController::class, 'edit'])->name('tasks.edit');
+       Route::patch('/{task}', [TaskController::class, 'update'])->name('tasks.update');
+       Route::delete('/{task}', [TaskController::class, 'delete'])->name('tasks.delete');
+   });
 });
