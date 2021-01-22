0eabfe5c618ed96cbb118c04c665942a18dc748f
David <david@docampo.ch>
2021-01-23 00:49:35 +0100

    added TaskController update method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 
 class TaskController extends Controller
@@ -29,7 +31,22 @@ class TaskController extends Controller
 
     }
 
-    public function update() {
+    public function update(UpdateTaskRequest $request, Task $task) {
 
+        if (!$request->complete) {
+            $request->complete = false;
+        }
+
+        $task->fill([
+            'title' => $request->title,
+            'description' => $request->description,
+            'complete' => $request->complete,
+            'due_at' => $request->due_at,
+            'user_id' => $request->user_id,
+        ]);
+
+        $task->save();
+
+        return redirect()->back()->with('success', 'Task successfully updated.');
     }
 }
