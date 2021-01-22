eedcf13b79939fa29c2d0a45286177a415cce8ce
David <david@docampo.ch>
2021-01-22 22:37:11 +0100

    added basic index page and index mehtod to controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class TaskController extends Controller
 {
-    //
+
+    public function index() {
+
+        return Inertia::render('Task/Index', [
+            'Tasks' => Task::where('user_id', Auth::user()->id)->paginate(),
+        ]);
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function delete() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function update() {
+
+    }
 }
--- resources/js/Pages/Task/Index.vue
+++ resources/js/Pages/Task/Index.vue
@@ -1,15 +1,33 @@
 <template>
   <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Tasks
+      </h2>
+    </template>
 
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div v-for="task in Tasks.data" class="bg-white overflow-hidden shadow-xl sm:rounded-lg mb-4">
+          <p v-text="task"></p>
+        </div>
+        <pagination :links="Tasks.links" />
+      </div>
+    </div>
   </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
+import Pagination from "@/Shared/Pagination";
 
 export default {
+    props: {
+        Tasks: Object,
+    },
     components: {
-        AppLayout
+        AppLayout,
+        Pagination,
     }
 }
 </script>
