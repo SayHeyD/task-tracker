f4b0f6e2001431d44a56e62500c1d9b2e4530405
David <david@docampo.ch>
2021-01-23 17:32:16 +0100

    added more task unit tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -22,6 +22,15 @@ class TaskTest extends TestCase
         return $user;
     }
 
+    private function sendValidTaskStoreRequest($title = 'Repair dishwasher') {
+
+        return $this->put('/tasks', [
+            'title' => $title,
+            'description' => 'Repair my dishwasher since my cat broke it.',
+            'due_at' => Carbon::now()->addDays(12)->toDateTimeString(),
+        ]);
+    }
+
     /** @test */
     public function tasks_index_returns_200_when_authenticated() {
 
@@ -109,5 +118,46 @@ class TaskTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors('due_at');
+    }
+
+    /** @test */
+    public function tasks_create_page_returns_200_when_authenticated() {
+
+        $this->authenticateUser();
+
+        $response = $this->get('tasks/new');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function tasks_create_page_returns_redirect_to_login_when_unauthenticated() {
+
+        $response = $this->get('tasks/new');
+
+        $response->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function tasks_store_returns_to_tasks_index_when_successful() {
+
+        $this->authenticateUser();
+
+        $this->sendValidTaskStoreRequest()->assertRedirect('/tasks');
+    }
+
+    /** @test */
+    public function tasks_store_has_success_message_when_request_is_successful() {
+
+        $this->authenticateUser();
+
+        $title = 'Some Title';
+
+        $this->sendValidTaskStoreRequest($title)->assertSessionHas('success', 'The Task "'.$title.'" was successfully created!');
+    }
+
+    /** @test */
+    public function tasks_store_returns_to_login_when_unauthenticated() {
+        $this->sendValidTaskStoreRequest()->assertRedirect('login');
     }
 }
