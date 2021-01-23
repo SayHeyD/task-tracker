8acd2c57e195529a5955d3e18efd40cc093c7d67
David <david@docampo.ch>
2021-01-23 16:18:07 +0100

    added id to jetstream input and label components

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -1,10 +1,10 @@
 <template>
-    <input class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+    <input :id="id" class="p-2 border border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:outline-none rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
 </template>
 
 <script>
     export default {
-        props: ['value'],
+        props: ['value', 'id'],
 
         methods: {
             focus() {
