42a4d93a01fdf0ebc9a51bd2357a65fb345a5f59
David <david@docampo.ch>
2021-01-23 15:37:09 +0100

    added delete confirmation modal

--- resources/js/Shared/Task/Card.vue
+++ resources/js/Shared/Task/Card.vue
@@ -20,6 +20,7 @@
 
     <div v-if="!collapsed">
       <p>Lorem ipsum</p>
+      <danger-button @click="showDeleteModal = true">Delete</danger-button>
     </div>
 
     <div>
@@ -28,18 +29,45 @@
         <svg v-else focusable="false" data-prefix="fas" data-icon="chevron-up" class="h-4 w-8 text-gray-500" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>
       </button>
     </div>
+
+    <confirmation-modal :show="showDeleteModal" @close="showDeleteModal = false">
+      <template #title>
+        Confirm Action
+      </template>
+
+      <template #content>
+        Are you sure you want to delete the task "{{ task.title }}"?
+      </template>
+
+      <template #footer>
+        <div class="flex justify-end">
+          <secondary-button class="mr-4" @click="showDeleteModal = false">Nevermind</secondary-button>
+          <danger-button @click="deleteTask">Delete</danger-button>
+        </div>
+      </template>
+    </confirmation-modal>
   </div>
 </template>
 
 <script>
+import DangerButton from "@/Jetstream/DangerButton";
+import ConfirmationModal from "@/Jetstream/ConfirmationModal";
+import SecondaryButton from "@/Jetstream/SecondaryButton";
+
 export default {
     props: {
         task: Object,
+    },
+    components: {
+        DangerButton,
+        ConfirmationModal,
+        SecondaryButton,
     },
     data() {
         return {
             collapsed: true,
             isUpdatingStatus: false,
+            showDeleteModal: false,
         }
     },
     methods: {
@@ -55,6 +83,13 @@ export default {
                     }
                 })
             }
+        },
+        deleteTask() {
+            this.$inertia.delete(route('tasks.delete', this.task.id), {
+                onFinish: () => {
+                    this.showDeleteModal = false
+                }
+            })
         },
         toggleCard() {
             this.collapsed = !this.collapsed;
