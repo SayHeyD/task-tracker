05294e88b25c9d23a4032b62f5bea6f91e60fcb1
David <david@docampo.ch>
2021-01-23 17:57:21 +0100

    added name to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         User::factory()->create([
-            'email' => 'david@docampo.ch'
+            'name' => 'David Docampo',
+            'email' => 'david@docampo.ch',
         ]);
         Task::factory(50)->create();
     }
