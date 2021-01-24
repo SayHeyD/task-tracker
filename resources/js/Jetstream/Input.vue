00e074ffc6421d061fd8764c6e0c4e7649371e98
David <david@docampo.ch>
2021-01-24 13:14:49 +0100

    added option to set type of input

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -1,10 +1,10 @@
 <template>
-    <input :id="id" class="p-2 border border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:outline-none rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
+    <input :id="id" :type="type" :placeholder="placeholder" class="p-2 border border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 focus:outline-none rounded-md shadow-sm" :value="value" @input="$emit('input', $event.target.value)" ref="input">
 </template>
 
 <script>
     export default {
-        props: ['value', 'id'],
+        props: ['value', 'id', 'placeholder', 'type'],
 
         methods: {
             focus() {
