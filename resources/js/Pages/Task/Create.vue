9b6e0468b170a8e5e6a75b17c28046d27136482f
David <david@docampo.ch>
2021-01-24 22:55:11 +0100

    created and used DateTimeInput

--- resources/js/Pages/Task/Create.vue
+++ resources/js/Pages/Task/Create.vue
@@ -21,7 +21,7 @@
             <template #form>
               <form-input id="title" class="w-full col-span-6" input-class="w-full" label="Title" type="text" :error-message="errors.title" v-model="form.title"></form-input>
               <form-input id="description" class="w-full col-span-6" input-class="w-full" label="Description" type="text" :error-message="errors.description" v-model="form.description"></form-input>
-              <label>Status</label>
+              <jet-label>Status</jet-label>
               <div class="w-full col-span-6 flex justify-around">
                 <button type="button" @click="form.status = 'completed'">
                   <svg :class="[form.status === 'completed' ? 'text-green-500 scale-110' : 'text-gray-300']" class="w-12 h-12 transition transform duration-250" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>
@@ -33,7 +33,7 @@
                   <svg :class="[form.status === 'open' ? 'text-gray-500 scale-110' : 'text-gray-300']" class="w-12 h-12 transition transform duration-250" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M256 8C119 8 8 119 8 256C8 393 119 504 256 504C393 504 504 393 504 256C504 119 393 8 256 8Z"/><path fill="white" d="M173.9 377.6C178.6 382.3 186.2 382.3 190.9 377.6L377.7 191C382.4 186.3 382.4 178.7 377.7 174L338.1 134.4C333.4 129.7 325.8 129.7 321.1 134.4L134.4 321C129.7 325.7 129.7 333.3 134.4 338L173.9 377.6Z"/></svg>
                 </button>
               </div>
-              <form-input id="due_at" class="w-full col-span-6" input-class="w-full" label="Due at" type="text" :error-message="errors.due_at" v-model="form.due_at"></form-input>
+              <date-time-input id="due_at" label="Due at" name="due_at" :error-message="errors.due_at" class="w-full col-span-6" v-model="form.due_at"></date-time-input>
             </template>
 
             <template #actions>
@@ -55,6 +55,8 @@ import AppLayout from "@/Layouts/AppLayout";
 import FormSection from "@/Jetstream/FormSection";
 import FormInput from "@/Shared/Form/FormInput";
 import SelectInput from "@/Shared/Form/SelectInput";
+import DateTimeInput from "@/Shared/Form/DateTimeInput";
+import JetLabel from "@/Jetstream/Label"
 import JetButton from "@/Jetstream/Button"
 
 export default {
@@ -64,6 +66,8 @@ export default {
         FormInput,
         JetButton,
         SelectInput,
+        DateTimeInput,
+        JetLabel,
     },
     props: {
         errors: Object,
---
+++ resources/js/Shared/Form/DateTimeInput.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="w-full flex flex-col justify-center">
+    <div class="flex w-full justify-between">
+      <jet-label :id="id" v-text="label"></jet-label>
+      <jet-input-error :message="errorMessage"></jet-input-error>
+    </div>
+    <div class="w-full flex justify-center">
+      <input :id="id" ref="flatpickr" class="hidden" :value="value"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import JetLabel from "@/Jetstream/Label";
+import JetInputError from "@/Jetstream/InputError";
+
+import flatpickr from "flatpickr";
+require('/css/flatpickr_blue.css')
+
+export default {
+    props: ['id', 'value', 'label', 'name', 'errorMessage'],
+    components: {
+        JetLabel,
+        JetInputError,
+    },
+    data() {
+        return {
+            options: {
+                inline: true,
+                dateFormat: 'Y-m-d H:i:s',
+                enableTime: true,
+                time_24hr: true,
+            }
+        }
+    },
+    mounted() {
+        this.date = this.value
+
+        let pickr = flatpickr(this.$refs.flatpickr, this.options);
+        pickr.config.onChange.push(this.emitInput);
+    },
+    methods: {
+        emitInput(val) {
+            let date = new Date(val[0])
+            let month = date.getMonth() + 1
+            let hour = date.getHours()
+            let day = date.getDate()
+            let minute = date.getMinutes()
+
+            if (minute.toString().length === 1) {
+                minute = '0' + minute.toString()
+            }
+
+            if (day.toString().length === 1) {
+                day = '0' + day.toString()
+            }
+
+            if (hour.toString().length === 1) {
+                hour = '0' + hour.toString()
+            }
+
+            if (month.toString().length === 1) {
+                month = '0' + month.toString()
+            }
+
+            this.$emit('input', date.getFullYear() + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':00')
+        }
+    },
+}
+</script>
