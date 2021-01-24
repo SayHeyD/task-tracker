0182b3b0c12e9a503b7b042ba4ea546cf789b403
David <david@docampo.ch>
2021-01-24 13:45:45 +0100

    adjusted pagination

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="mt-6 -mb-1 flex flex-wrap">
     <template v-for="(link, key) in links">
-      <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded text-gray-400" :class="{ 'ml-auto': link.label === 'Next' }">{{ link.label }}</div>
-      <inertia-link v-else :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border rounded hover:bg-white focus:border-blue-500 focus:text-blue-500" :class="{ 'bg-white': link.active, 'ml-auto': link.label === 'Next' }" :href="link.url">{{ link.label }}</inertia-link>
+      <div v-if="link.url === null" :key="key" class="mr-1 mb-1 px-4 py-3 text-sm border bg-white rounded text-gray-400" :class="{ 'ml-auto': link.label === 'Next', 'cursor-not-allowed': link.url === null }">{{ link.label }}</div>
+      <inertia-link v-else :key="key"
+                    class="mr-1 mb-1 px-4 py-3 text-sm border rounded bg-white hover:border-blue-500 focus:border-blue-500 focus:text-blue-500"
+                    :class="{ 'bg-white cursor-not-allowed border-blue-500 text-blue-500': link.active || link.url === null, 'ml-auto': link.label === 'Next'}"
+                    :href="link.url">{{ link.label }}</inertia-link>
     </template>
   </div>
 </template>
